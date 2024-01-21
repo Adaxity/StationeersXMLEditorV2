b125467a3a654d3d0d66fe2cd1672a332d1f130e
Vít Otáhal <59264209+Adaxity@users.noreply.github.com>
2024-01-21 02:29:24 +0100

    h

--- SXMLE2/Program.cs
+++ SXMLE2/Program.cs
@@ -6,13 +6,13 @@ internal static class Program
 		ApplicationConfiguration.Initialize();
 
 #if DEBUG
-		SXMLE2.exeDir = @"C:\Program Files (x86)\Steam\steamapps\common\Stationeers";
-		SXMLE2.configDir = SXMLE2.exeDir + @"\SXMLE_Configs";
+		SXMLE2.exeDir = @"C:\Program Files (x86)\Steam\steamapps\common\Stationeers\";
+		SXMLE2.configDir = SXMLE2.exeDir + @"\SXMLE_Configs\";
 #endif
 
-		if (SXMLE2.IsInCorrectDirectory)
+		if (!SXMLE2.IsInCorrectDirectory)
 			Application.Run(new SXMLE2());
 		else
-			MessageBox.Show($"Please put the .exe file into your Stationeers folder.\n\nCurrently in: {SXMLE2.exeDir}", "Incorrect file location", MessageBoxButtons.OK);
+			MessageBox.Show($"Please put the .exe file into your Stationeers folder.\n\nCurrently in: {SXMLE2.exeDir} ({Path.GetFileNameWithoutExtension(SXMLE2.exeDir)}, {SXMLE2.exeDir})", "Incorrect file location", MessageBoxButtons.OK);
 	}
 }
